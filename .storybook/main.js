f60b357811d08a7f9b03d927ac22252ff413d754
Nitish Ranjan <nitish.ranjan@ihsmarkit.com>
2021-02-19 18:42:48 +0530

    Code First

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "stories": [
+    "../src/app/**/*.stories.mdx",
+    "../src/app/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    '@storybook/addon-actions', 
+    '@storybook/addon-notes'
+  ]
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+//src/app/app.component.ts
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <app-inbox-screen></app-inbox-screen>
+  `,
+})
+export class AppComponent {
+  title = 'taskbox';
+}
